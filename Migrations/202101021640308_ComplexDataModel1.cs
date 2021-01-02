1cf297aec18602a86bd6098c92027792cfa30535
Jon-Flan <52779521+Jon-Flan@users.noreply.github.com>
2021-01-02 16:45:56 +0000

    DB String  changes, published version

---
+++ Migrations/202101021640308_ComplexDataModel1.cs
@@ -0,0 +1,16 @@
+﻿namespace Booking_App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ComplexDataModel1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
